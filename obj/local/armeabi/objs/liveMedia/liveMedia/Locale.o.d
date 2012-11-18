9a995737307c6e89e10b531db3f07acc5ae4cd8f
Sumit Ahlawat <ahlawat@gmail.com>
2012-11-18 23:38:46 +0800

    remove unused files

--- obj/local/armeabi/objs/liveMedia/liveMedia/Locale.o.d
+++
@@ -1,8 +0,0 @@
-/home/sumit/Downloads/playstream/playstream/obj/local/armeabi/objs/liveMedia/liveMedia/Locale.o: \
- /home/sumit/Downloads/playstream/playstream/jni/liveMedia/Locale.cpp \
- /home/sumit/Downloads/playstream/playstream/jni/liveMedia/include/Locale.hh \
- /home/sumit/Downloads/playstream/playstream/jni/UsageEnvironment/include/strDup.hh
-
-/home/sumit/Downloads/playstream/playstream/jni/liveMedia/include/Locale.hh:
-
-/home/sumit/Downloads/playstream/playstream/jni/UsageEnvironment/include/strDup.hh:
